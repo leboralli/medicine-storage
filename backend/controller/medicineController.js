62cb484fdc374ff2f3cb38263d9cbc7e52179554
Leandro A F Boralli <leboralli@gmail.com>
2023-02-21 19:04:52 -0500

    Controller working

--- backend/controller/medicineController.js
+++ backend/controller/medicineController.js
@@ -1,21 +1,22 @@
-const { Service: MedicineModel } = require('../models/medicineModel');
+const MedicineModel = require('../models/medicineModel');
 
 const medicineController = {
-  create: async (req, res) => {
+  create: async (req, res) => {  
     try {
-      const medicine = {
-        name: req.body.name,
-        quantity: req.body.quantity,
-        description: req.body.description,
-      };
-      console.log(medicine);
-      const newMedicine = await MedicineModel.create(medicine);
-      res.status(201).json({ newMedicine, message: "Serviço criado com sucesso" });
+        console.log(MedicineModel);
+        const medicine = {
+          name: req.body.name,
+          quantity: req.body.quantity,
+          description: req.body.description,
+        };
+        console.log(medicine);
+        const newMedicine = await MedicineModel.create(medicine);
+        res.status(201).json({ newMedicine, message: "Serviço criado com sucesso" });
 
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  }
+      } catch (err) {
+        res.status(400).json({ message: err.message });
+      }
+  },
 };
 
 module.exports = medicineController;
