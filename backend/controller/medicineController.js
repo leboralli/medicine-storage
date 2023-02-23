648c9bd83468b0cd718a8213398598c9dd95bd51
Leandro A F Boralli <leboralli@gmail.com>
2023-02-22 19:16:07 -0500

    all CRUD functions

--- backend/controller/medicineController.js
+++ backend/controller/medicineController.js
@@ -3,7 +3,6 @@ const MedicineModel = require('../models/medicineModel');
 const medicineController = {
   create: async (req, res) => {  
     try {
-        console.log(MedicineModel);
         const medicine = {
           name: req.body.name,
           quantity: req.body.quantity,
@@ -16,6 +15,55 @@ const medicineController = {
       } catch (err) {
         res.status(400).json({ message: err.message });
       }
+  },
+  findAll: async (req, res) => {
+    try {
+      const medicines = await MedicineModel.find();
+      res.status(200).json(medicines);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+  findOne: async (req, res) => {
+    try {
+      const medicine = await MedicineModel.findById(req.params.id);
+      if (!medicine) {
+        return res.status(404).json({ message: 'Medicine not found' });
+      }
+      res.status(200).json(medicine);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const medicine = await MedicineModel.findByIdAndUpdate(
+        req.params.id,
+        {
+          name: req.body.name,
+          quantity: req.body.quantity,
+          description: req.body.description
+        },
+        { new: true }
+      );
+      if (!medicine) {
+        return res.status(404).json({ message: err.message });
+      }
+      res.status(200).json(medicine);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const medicine = await MedicineModel.findByIdAndDelete(req.params.id);
+      if (!medicine) {
+        return res.status(404).json({ message: err.message });
+      }
+      res.status(200).json({ message: "Medicine deleted successfully" });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
   },
 };
 
--- backend/routes/medicine.js
+++ backend/routes/medicine.js
@@ -2,7 +2,28 @@ const router = require('express').Router();
 
 const medicineController = require('../controller/medicineController');
 
+// Create a new medicine
 router
   .route('/list')
   .post((req, res) => medicineController.create(req, res));
+
+// Retrieve all medicines
+router
+  .route('/list')
+  .get((req, res) => medicineController.findAll(req, res));
+  
+// Retrieve a single medicine with id
+router
+  .route('/list/:id')
+  .get((req, res) => medicineController.findOne(req, res));
+  
+// Update a medicine with id
+router
+  .route('/list/:id')
+  .put((req, res) => medicineController.update(req, res));
+  
+// Delete a medicine with id
+router
+  .route('/list/:id')
+  .delete((req, res) => medicineController.delete(req, res));
 module.exports = router;
